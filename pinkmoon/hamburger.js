c3158ccf9307c9b46990e79cf34f2216eb2d62f3
samueldhoore <55515834+samueldhoore@users.noreply.github.com>
2022-10-25 17:40:08 +0200

    script

---
+++ pinkmoon/hamburger.js
@@ -0,0 +1,8 @@
+function hamburgerFunction() {
+  var x = document.getElementById("toplinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+} 
