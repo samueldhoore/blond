8013bfe9d4bc9a4e6beb4a73fd48aaf93fcb39ca
samueldhoore <55515834+samueldhoore@users.noreply.github.com>
2022-10-25 17:44:07 +0200

    by class

--- pinkmoon/hamburger.js
+++ pinkmoon/hamburger.js
@@ -1,5 +1,5 @@
 function hamburgerFunction() {
-  var x = document.getElementById("toplinks");
+  var x = document.getElementByClass("toplinks");
   if (x.style.display === "block") {
     x.style.display = "none";
   } else {
